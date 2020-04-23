3d678ecb05af9cf5df0791dc5746575f402f4d59
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2020-04-22 19:29:36 -0600

    added team

--- Text book follow along/app.js
+++ Text book follow along/app.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url == '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter message</title><head>');
+        res.write('<body><from action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
