b9ea74d1f8dd380cc5ea4831060dfc112a595060
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2020-04-25 20:17:22 -0600

    prove

---
+++ Prove/week1.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+ 
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    const activityList = [];
+    if (url === '/') {
+       res.write('<html>');
+       res.write('<head><title>Activities Page</title></head>');
+       res.write('<body>');
+       res.write('<h1>Please enter your favorite two activities</h1>');
+       res.write('<form action="/add-activity" method="POST">');
+       res.write('<input type="text" name="message"><br><br>');
+       res.write('<input type="text" name="mess"><br><br>');
+       res.write('<button type="submit">Submit</button></form>');
+       res.write('</body>');
+       res.write('</html>');
+       return res.end();
+    }
+    if (url === '/add-activity' && method === 'POST') {
+       const body = [];
+       req.on('data', (info) => {
+           body.push(info);
+       });
+       req.on('end',() => {
+           activityList.push(Buffer.concat(body).toString());
+           console.log(activityList);
+       })
+       res.write('<html>');
+       res.write('<head><title>Activities Page</title></head>');
+       res.write('<body>');
+       res.write('<h1>Possible Activities</h1>')
+       res.write('<form action="/add-activity" method="POST">');
+       res.write('<ol>');
+       for (item of activityList) {
+            res.write('<li>' + item + '</li>');
+       }
+       res.write('</ol>');
+       res.write('Add activity<br>')
+       res.write('<input type="text" name="message"><br>');
+       res.write('<button type="submit">Submit</button></form>');
+       res.write('</body>');
+       res.write('</html>');
+       return res.end();
+    };
+});
+server.listen(3000);
